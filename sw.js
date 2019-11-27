14eab4c3c11ac89453e2cd7f15b60e1675fcf049
Omer Faruk KAAN <omerfaruk.kaan@stu.fsm.edu.tr>
2019-11-27 19:29:54 +0300

    PWA

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE ='advancedProgramming'
+const FILES = ['/advancedProgramming/Classworks/CW1/PieChart.png','/advancedProgramming/Classworks/CW2/Counting.html', '/advancedProgramming/Classworks/CW2/TemperatureConvertor.html', 
+'/advancedProgramming/Classworks/CW3/c4_data.html', '/advancedProgramming/Classworks/CW4/Courses.html', '/advancedProgramming/Classworks/CW5/Eloquent_JS.html'
+,'/advancedProgramming/Classworks/CW6/File_Operations.html', '/advancedProgramming/Classworks/CW7/Array_vs_Map.html', 
+'/advancedProgramming/Homeworks/HW1/Poisson_Table.html',
+'/advancedProgramming/Homeworks/HW2/Database.html','/advancedProgramming/Homeworks/HW3/JS_Animations.html']
+
+function installCB(e) {
+  e.waitUntil(
+    caches.open(CACHE)
+    .then(cache => cache.addAll(FILES))
+    .catch(console.log)
+  )
+}
+self.addEventListener('install', installCB)
+
+function save(req, resp) {
+  return caches.open(CACHE)
+  .then(cache => {
+    cache.put(req, resp.clone());
+    return resp;
+  })
+  .catch(console.log)
+}
+function fetchCB(e) { //fetch first
+  let req = e.request
+  console.log('advancedProgramming', req.url);
+  e.respondWith(
+    fetch(req).then(r2 => save(req, r2))
+    .catch(() => { return caches.match(req).then(r1 => r1) })
+  )
+}
+self.addEventListener('fetch', fetchCB)
